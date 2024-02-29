326c6838c36bb4757e0f9794f125e8568d5283f9
Leito <leito2@github.com>
2024-02-28 23:37:09 -0500

    Ejercicios Resueltos

---
+++ Ejercicio14/src/ejercicio14/Ejercicio14.java
@@ -0,0 +1,13 @@
+package ejercicio14;
+
+public class Ejercicio14 {
+    public static void main(String[] args) {
+        int numero = 4;
+        double cuadrado, cubo;
+        cuadrado = Math.pow(numero, 2);
+        cubo = Math.pow(numero, 3);
+        System.out.println("Numero: " + numero);
+        System.out.println("Cuadrado: " + cuadrado);
+        System.out.println("Cubo: " + cubo);
+    }
+}
